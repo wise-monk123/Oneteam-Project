6c5a22a72f6828c497628fad52e1a26fdc2db33d
wise-monk <yuhua.he@sjsu.edu>
2018-11-09 18:41:40 -0800

    setup coverage limit

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -31,6 +31,14 @@ module.exports = function karmaConfig(config) {
         { type: 'lcov', subdir: '.' },
         { type: 'text-summary' },
       ],
+      check: {
+        global: {
+          statements: 50,
+          branches: 50,
+          functions: 50,
+          lines: 50,
+        },
+      },
     },
   });
 };
