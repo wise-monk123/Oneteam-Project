15b717bc6fa508ac8e9021a80794f4182e29bc05
wise-monk <yuhua.he@sjsu.edu>
2018-11-09 19:36:02 -0800

    coverage

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -33,10 +33,10 @@ module.exports = function karmaConfig(config) {
       ],
       check: {
         global: {
-          statements: 0,
-          branches: 0,
-          functions: 0,
-          lines: 0,
+          statements: 50,
+          branches: 50,
+          functions: 50,
+          lines: 50,
         },
       },
     },
