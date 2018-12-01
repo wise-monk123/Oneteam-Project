10e53e8f1db1b8a34a3df670d37cac93a27c8ecb
wise-monk <yuhua.he@sjsu.edu>
2018-12-01 13:45:13 -0800

    integrate with task lane

---
+++ src/components/board/TaskLaneItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="card task-lane-item">
+    <div class="card-block">
+      <h5 class="card-title">
+        <span class="text-muted">#T{{item.id}}</span>
+        {{item.text}}
+      </h5>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TaskLaneItem',
+  props: {
+    item: {
+      type: Object,
+      default: (() => {}),
+    },
+  },
+};
+</script>
+
+<style>
+  .card.task-lane-item {
+    background: #70d662;
+  }
+</style>
